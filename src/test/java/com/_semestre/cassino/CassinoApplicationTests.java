d121d126bb8d1daddef04444163f607f889ddd8a
jair carlos <Jair61504@gmail.com>
2025-06-12 03:59:14 -0300

    first commit

---
+++ src/test/java/com/_semestre/cassino/CassinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._semestre.cassino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
